adc25c4e9acbe012a7da73c5fa4d856909b64847
Kenny Phan <tul19699@temple.edu>
2022-09-21 02:29:28 -0400

    Base

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 movementInput;
-    Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
