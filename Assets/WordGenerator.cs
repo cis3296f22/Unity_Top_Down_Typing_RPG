dc815b3084126baae8c46f67dac5ecbfba4f9c0e
Ethan Hopkins <tuk01901@temple.edu>
2022-11-09 16:32:41 -0500

    added functionality to generate words from huge dictionary based on wordlength, does not like it when you type longer than the sentence length

--- Assets/WordGenerator.cs
+++ Assets/WordGenerator.cs
@@ -2,20 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.IO;
+using Random=UnityEngine.Random;
 
 public class WordGenerator : MonoBehaviour
 {
-    private static string[] words = new string[369651];
+    private static int dictLen = 369652;
+    private static string[] words = new string[dictLen];
 
-    public static string GenerateSentence()
+    public static void GenerateDict()
     {
-        int minlength = 1, maxlength = 7;
+        
         System.Random rand = new System.Random();
         string path = "Assets/wordsByLen.txt";
         StreamReader reader = new StreamReader(path);
-        o
+        string line;
+        // Read and display lines from the file until the end of
+        // the file is reached.
+        int i = 0;
+        while ((line = reader.ReadLine()) != null)
+        {
+            words[i]= line.TrimEnd('\n');
+            i++;
+        }
+    }
 
-        int index = rand.Next(3);
-        return sentences[index];
+    public static String GenerateSentence(){
+        //print(word);
+        int sentenceLen= 10;
+        int minlength = 2, maxlength = 7;
+        string sentence = "";
+        for(int i = 0; i< sentenceLen; i++){
+            int num = (int)Random.Range(0, dictLen);
+            while(words[num].Length<minlength||words[num].Length>maxlength) 
+            {
+                num = Random.Range(0, dictLen);
+            }
+            sentence+= words[num];
+            sentence+= " ";
+        }
+        return sentence;
     }
 }
